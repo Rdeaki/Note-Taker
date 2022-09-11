f80661d124b82acd5cd049b7f7c50adbfc4f3290
rick Deakins <rdeakins96@gmail.com>
2022-09-11 15:22:16 -0400

    add middleware

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const data = require('./db/db.json');
-
+const path = require('path');
 
 const app = express();
+//middleware
+app.use(express.static('public'))
 
-app.get('/html', (req, res) =>{
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 app.get('/notes', (req, res)=>{
